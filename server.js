fa202560b131bf855b65eecbc4d7e70b5be79112
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-29 10:36:59 -0500

    updated db functions like add/delete/ call all/ and get for single candidate

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // required to connect the MySQL database
 const mysql = require('mysql2');
 const express = require('express');
-const PORT = process.env.PORT|| 3001;
+const { isBuffer } = require('util');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -15,8 +18,7 @@ app.get('/', (req, res) => {
 });
 
 //also needed with top const to connect to MySQL
-const db = mysql.createConnection( 
-    {
+const db = mysql.createConnection({
         host: 'localhost',
         //your MySQL username
         user: 'root',
@@ -28,8 +30,36 @@ const db = mysql.createConnection(
 );
 
 //catchall routes to test database connection
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+/*db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
+});
+*/
+
+//GET route for a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+// if(err) {
+// console.log(err);
+// }
+// console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// if(err) {
+// console.log(err);
+// }
+// console.log(result);
+// });
+
+//Create a Candidate params of id first, last, connections all must match the params array below, the 4 ? are placeholders of the params until the params array is created
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                        VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //default response for any other request(Not Found)
