f26d422be9310231c8d228f5f8d0cdb4575c9558
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-29 11:49:56 -0500

    updated the api endroutes, function not working properly

--- server.js
+++ server.js
@@ -1,73 +1,116 @@
-// required to connect the MySQL database
-const mysql = require('mysql2');
 const express = require('express');
-const { isBuffer } = require('util');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//middleware
-app.use(express.urlencoded({
-    extended: false
-}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
-
-//also needed with top const to connect to MySQL
-const db = mysql.createConnection({
-        host: 'localhost',
-        //your MySQL username
-        user: 'root',
-        //Your Mysql password
-        password: '8410Ble$$',
-        database: 'election'
-    },
-    console.log('Connected to the election Database')
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
 );
 
-//catchall routes to test database connection
-/*db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
-*/
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
 
-//GET route for a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-// if(err) {
-// console.log(err);
-// }
-// console.log(row);
-// });
-
-//Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-// if(err) {
-// console.log(err);
-// }
-// console.log(result);
-// });
-
-//Create a Candidate params of id first, last, connections all must match the params array below, the 4 ? are placeholders of the params until the params array is created
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-                        VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-db.query(sql, params, (err, result) => {
-    if(err) {
-        console.log(err);
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
     }
-    console.log(result);
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 
-//default response for any other request(Not Found)
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-//function that allows this app to listen and tell it to run
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
